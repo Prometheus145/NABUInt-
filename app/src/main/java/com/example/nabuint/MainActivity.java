333917c87bc67dcb62e9725c574eadca45552bbc
Prometheus145 <66186010+Prometheus145@users.noreply.github.com>
2020-07-18 19:50:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nabuint/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.nabuint;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem tab1,tab2,tab3,tab4;
+    public PageAdapter pageradapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tab1 = (TabItem) findViewById(R.id.Tab1);
+        tab2 = (TabItem) findViewById(R.id.Tab2);
+        tab3 = (TabItem) findViewById(R.id.Tab3);
+        tab4 = (TabItem) findViewById(R.id.Tab4);
+        viewPager = findViewById(R.id.viewpager);
+
+        pageradapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageradapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    pageradapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 1) {
+                    pageradapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 3) {
+                    pageradapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 4) {
+                    pageradapter.notifyDataSetChanged();
+                }
+            }
+
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+    }}
---
+++ app/src/main/java/com/example/nabuint/PageAdapter.java
@@ -0,0 +1,44 @@
+package com.example.nabuint;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private int numberoftabs;
+
+    public PageAdapter(@NonNull FragmentManager fm, int numberoftabs) {
+        super(fm);
+        this.numberoftabs = numberoftabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new Tab1();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab3();
+            case 3:
+                 return new Tab4();
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numberoftabs;
+    }
+
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}

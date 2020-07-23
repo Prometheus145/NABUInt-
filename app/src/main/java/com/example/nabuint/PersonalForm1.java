0c89bd0ba5cb7927832509ca7f3db2e4c4910580
William Huang <williamhuang314@gmail.com>
2020-07-23 02:04:30 -0400

    Personal risk eval form additions

--- app/src/main/java/com/example/nabuint/PersonalForm1.java
+++ app/src/main/java/com/example/nabuint/PersonalForm1.java
@@ -4,12 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 public class PersonalForm1 extends AppCompatActivity {
+
+    private String ageRange = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_form1);
 
+        Spinner ageSpinner = findViewById(R.id.ageSpinner);
+
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this, R.array.ageSpinner,
+                android.R.layout.simple_spinner_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+        ageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ageRange = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(adapterView.getContext(), "Please complete every question.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
